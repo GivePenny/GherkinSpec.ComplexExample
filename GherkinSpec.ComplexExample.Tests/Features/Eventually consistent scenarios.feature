74d431ba29fdd46708d6916b686ffd1de0b314a5
Carl Hartshorn <carl.hartshorn@gmail.com>
2019-03-14 17:54:28 +0000

    Add example of test scenarios tagged eventuallyConsistent

---
+++ GherkinSpec.ComplexExample.Tests/Features/Eventually consistent scenarios.feature
@@ -0,0 +1,20 @@
+﻿Feature: Eventually consistent scenarios
+
+@eventuallyConsistent
+Scenario: Eventually consistent scenario
+	Given data has been set up
+	When an action is performed
+	Then the data setup step should only ever be called once
+	And the action performed step should be called three times
+
+@eventuallyConsistent
+Scenario Outline: Eventually consistent scenario outline
+	Given data has been set up
+	When an action is performed
+	Then the data setup step should only ever be called once
+	And the action performed step should be called <count> times
+Examples:
+    | count |
+    | 1     |
+    | 2     |
+    | 3     |
---
+++ GherkinSpec.ComplexExample.Tests/Steps/EventuallyConsistentScenariosSteps.cs
@@ -0,0 +1,32 @@
+﻿using GherkinSpec.TestModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GherkinSpec.ComplexExample.Tests.Steps
+{
+    [Steps]
+    public class EventuallyConsistentScenariosSteps
+    {
+        private int givenCalls;
+        private int whenCalls;
+
+        [Given("data has been set up")]
+        public void GivenDataHasBeenSetUp()
+            => givenCalls++;
+
+        [When("an action is performed")]
+        public void WhenAnActionIsPerformed()
+            => whenCalls++;
+
+        [Then("the data setup step should only ever be called once")]
+        public void ThenTheDataSetupStepShouldOnlyEverBeCalledOnce()
+            => Assert.AreEqual(1, givenCalls);
+
+        [Then("the action performed step should be called three times")]
+        public void ThenTheActionPerformedStepShouldBeCalledThreeTimes()
+            => Assert.AreEqual(3, whenCalls);
+
+        [Then("the action performed step should be called (\\d) times")]
+        public void ThenTheActionPerformedStepShouldBeCalledCountTimes(int count)
+            => Assert.AreEqual(count, whenCalls);
+    }
+}
